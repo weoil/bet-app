2de4fd825dd98f7ebc72a36da83cb55f3120469f
weoil <weoil@live.cn>
2019-08-20 21:12:18 +0800

    modal

--- src/coms/modal/modal.vue
+++ src/coms/modal/modal.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="i-modal"></div>
+  <div class="i-modal" v-if="show" :class="{ visible: status }">
+    <div class="wrapper">
+      <div class="title">
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </div>
+      <div class="content">
+        <slot></slot>
+      </div>
+      <div class="footer" v-if="isShowFooter">
+        <slot name="footer">
+          <button class="button cancel" v-if="isShowCancel">
+            {{ cancelText }}
+          </button>
+          <button class="button confirm" v-if="isShowConfirm">
+            {{ confirmText }}
+          </button>
+        </slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang='ts'>
-import { Vue, Component, Prop, Model, Watch } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 @Component({
   name: 'i-modal',
 })
@@ -13,10 +34,55 @@ class App extends Vue {
     default: false,
   })
   status!: boolean;
+  @Prop({
+    type: String,
+    default: '',
+  })
+  title!: string;
+
+  @Prop({
+    type: String,
+    default: '取消',
+  })
+  cancelText!: string;
+  @Prop({
+    type: String,
+    default: '确认',
+  })
+  confirmText!: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  isShowConfirm!: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  isShowCancel!: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  isShowFooter!: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  mask!: boolean;
+
   show: boolean = this.status;
+  delay: number = 500;
   @Watch('status')
   onStatusChange(val: boolean) {
     this.show = val;
+  }
+  onCancel() {
+    this.$emit('update:status', false);
   }
   // mounted(){}
   // beforeMount(){}
@@ -26,5 +92,7 @@ export default App;
 
 <style lang="scss" scoped>
 .i-modal {
+  &.visible {
+  }
 }
 </style>
